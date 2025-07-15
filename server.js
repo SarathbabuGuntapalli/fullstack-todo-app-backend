346d7a5c120b92de79cc2d73a68783f88a35cb84
Sarath Babu G <104516120+SarathbabuGuntapalli@users.noreply.github.com>
2025-07-15 12:48:23 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const route = require("./routes/todoRoute");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.port || 8000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Mongo connected..."))
+  .catch((err) => console.log(err));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// Then add body parser and routes
+app.use(express.json());
+app.use(route);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
